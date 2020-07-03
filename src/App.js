40e5cde252810af306ada674c02be7c5b9024775
Santiago Herrera <santiago.herreragomez@gmail.com>
2020-07-02 23:12:26 -0500

    App modified to use JSX (react html)

--- src/App.js
+++ src/App.js
@@ -3,23 +3,32 @@ import { render } from 'react-dom'
 import Pet from './Pet.jsx';
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet, {
-      name: "Moon",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed",
-    }),
-  ]);
-};
-render(React.createElement(App), document.getElementById("root"));
+//   return React.createElement("div", { id: "something-important" }, [
+//     React.createElement("h1", {}, "Adopt me!"),
+//     React.createElement(Pet, {
+//       name: "Moon",
+//       animal: "Dog",
+//       breed: "Havanese",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Pepper",
+//       animal: "Bird",
+//       breed: "Cockatiel",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Doink",
+//       animal: "Cat",
+//       breed: "Mixed",
+//     }),
+//   ]);
+// };
+  return (
+    <div id="something-important">
+      <h1>Adopt me!</h1>
+      <Pet name="Moon" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel, " />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  )
+}
+render(<App />, document.getElementById("root"));
