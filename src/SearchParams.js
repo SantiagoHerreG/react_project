629f7cdbaccb5f2c687de7926819a9a97bb4489f
Santiago Herrera <santiago.herreragomez@gmail.com>
2020-07-04 20:06:36 -0500

    Dropdown component created to abstract all select tags using hooks

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { ANIMALS } from '@frontendmasters/pet';
+import useDropdown from './useDropdown';
 
 const SearchParams = () => {
     
     const [location, setLocation] = useState("Seattle, WA");
-    const [animal, setAnimal] = useState("dog");
-    const [breed, setBreed] = useState("");
     const [breeds, setBreeds] = useState([]);
+    const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+    const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);    
 
-    
     return (
         <div className="search-params">
             <form>
@@ -21,7 +21,9 @@ const SearchParams = () => {
                         onChange={ e => setLocation(e.target.value) }
                     />
                 </label>
-                <label htmlFor="animal">
+                <AnimalDropdown />
+                <BreedDropdown />
+                {/* <label htmlFor="animal">
                     Animal
                     <select
                         id="animal"
@@ -53,7 +55,7 @@ const SearchParams = () => {
                             ))
                         }                        
                     </select>
-                </label>
+                </label> */}
                 <button>Submit</button>
             </form>
         </div>
