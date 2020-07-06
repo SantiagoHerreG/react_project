65f931d097c2382185c1c82af9d009fe5e1c8034
Santiago Herrera <santiago.herreragomez@gmail.com>
2020-07-05 21:37:53 -0500

    prettier format added, new component for pets, with image uploaded and image placeholder from placecorgi.com

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import { render } from 'react-dom'
-import Pet from './Pet.jsx';
-import SearchParams from './SearchParams.js';
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
+import SearchParams from "./SearchParams.js";
 
 const App = () => {
-//   return React.createElement("div", { id: "something-important" }, [
-//     React.createElement("h1", {}, "Adopt me!"),
-//     React.createElement(Pet, {
-//       name: "Moon",
-//       animal: "Dog",
-//       breed: "Havanese",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Pepper",
-//       animal: "Bird",
-//       breed: "Cockatiel",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Doink",
-//       animal: "Cat",
-//       breed: "Mixed",
-//     }),
-//   ]);
-// };
+  //   return React.createElement("div", { id: "something-important" }, [
+  //     React.createElement("h1", {}, "Adopt me!"),
+  //     React.createElement(Pet, {
+  //       name: "Moon",
+  //       animal: "Dog",
+  //       breed: "Havanese",
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Pepper",
+  //       animal: "Bird",
+  //       breed: "Cockatiel",
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Doink",
+  //       animal: "Cat",
+  //       breed: "Mixed",
+  //     }),
+  //   ]);
+  // };
   return (
     <div id="something-important">
       <h1>Adopt me!</h1>
-      <Pet name="Moon" animal="Dog" breed="Havanese" />
+      {/* <Pet name="Moon" animal="Dog" breed="Havanese" />
       <Pet name="Pepper" animal="Bird" breed="Cockatiel, " />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" /> */}
       <SearchParams />
     </div>
-  )
-}
+  );
+};
 render(<App />, document.getElementById("root"));
