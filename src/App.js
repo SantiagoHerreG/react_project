7613693c1d50cbce9dd079aa09dfd583ebcff292
Santiago Herrera <santiago.herreragomez@gmail.com>
2020-07-12 23:06:11 -0500

    new files added for error boundaries and modals, using portals

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams.js";
 import Details from "./Details.js";
+import ThemeContext from './themeContext';
 
 const App = () => {
   //   return React.createElement("div", { id: "something-important" }, [
@@ -24,17 +25,20 @@ const App = () => {
   //     }),
   //   ]);
   // };
+  const themeHook = useState('yellow');
   return (
     <React.StrictMode>
-      <div>
-        <header>
-          <Link to="/">Adopt me!</Link>
-        </header>
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
-      </div>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/">Adopt me!</Link>
+          </header>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
   );
 };
